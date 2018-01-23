64967510b208782ecf96009b8e66ebce793ba7f8
kbsonlong <kbsonlong@gmail.com>
2018-01-23 14:14:33 +0800

    博文爬取上传到个人wordpress博客站点

--- old_csdn.py
+++ old_csdn.py
@@ -1,6 +1,8 @@
 #-*- coding:utf-8 -*-
 #!/ust/bin/env python
-##用于csdn新版爬取
+
+##用于csdn旧版爬取
+
 from bs4 import BeautifulSoup
 from wppost import *
 
@@ -30,27 +32,23 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 def get_links(url):
     soup = BeautifulSoup(get_info(url),'html.parser')
     hrefs =[]
-    for h in soup.find('ul',{'class':'blog-units blog-units-box'}).findAll('a'):
-        href =host + h.get('href')
+    for h in soup.findAll('span', {'class': 'link_title'}):
+        href =host + h.a.get('href')
         hrefs.append(href)
     return hrefs
 
 
-
-
-def get_context(url,tags='',category=''):
-    links = get_links(url)
+def get_context(links,tags='',category=''):
     news = []
     for url in links:
         soup = BeautifulSoup(get_info(url), 'html.parser')
-        print
         try:
-            title = soup.find('h1',{'class':'csdn_top'}).get_text()
+            title = soup.find('span',{'class':'link_title'}).a.get_text()
             soup_context = soup.find('div', {
-                'class': 'article_content csdn-tracking-statistics tracking-click'})
+                'class': 'article_content csdn-tracking-statistics'})
             ###删除script标签，很多时候爬取内容中带有内嵌的广告script
             # [s.extract() for s in soup_context('script')]
-            context = "%s \n 本文转载自 <a href='%s'> %s</a> " % (str(soup_context), str(url), str(title))
+            context = "%s \n 本文转载自 <a href='%s' targe='_blank'> %s</a> " % (str(soup_context), str(url), str(title))
             image_name = ''
             news.append(Contexts(title, tags, category, context, image_name))
         except Exception as e:
@@ -60,11 +58,14 @@ def get_context(url,tags='',category=''):
     return news
 
 if __name__ == '__main__':
-    url = 'http://blog.csdn.net/liumiaocn/article/category/6334627'
-    news = get_context(url,category='Ansible',tags='Ansible')
+    url = 'http://blog.csdn.net/huwh_/article/category/7158816'
+    links = get_links(url)
+    # links=['http://blog.csdn.net/huwh_/article/details/71308171']
+    news = get_context(links,category='Docker',tags='Kubernetes')
+    # print get_context(links)
     try:
         for new in news:
-            user = {'website': 'http://www.along.party/xmlrpc.php', 'username': 'xxxx', 'password': 'xxxxxx'}
+            user = {'website': 'http://www.along.party/xmlrpc.php', 'username': 'xxx', 'password': 'xxxxx'}
             send_news(user,new)
     except Exception as e:
         print traceback.format_exc()
